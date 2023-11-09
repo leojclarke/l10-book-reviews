11923febe97e22bb07c775adc61e2dcc097465c4
Leo Clarke <leojclarke@gmail.com>
2023-11-09 12:34:36 +0100

    Fix use of `Builder` class form different name spaces

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -20,11 +21,11 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title .'%');
     }
 
-    public function scopePopular(Builder $query): Builder {
+    public function scopePopular(Builder $query): Builder|QueryBuilder {
         return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder {
+    public function scopeHighestRated(Builder $query): Builder|QueryBuilder {
         return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
