9477b1a3386e9b95c58bee56b105e79ba142c208
Leo Clarke <leojclarke@gmail.com>
2023-11-12 15:12:25 +0100

    Add search function to books page

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,8 +5,12 @@
         Books
     </h1>
 
-    <form>
+    <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+        @csrf
+        <input type="text" class="input h-10" name="title" placeholder="Search by title" value="{{ request('title') }}" />
 
+        <button type="submit" class="btn">Search</button>
+        <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
 
     <ul>
@@ -15,7 +19,7 @@
                 <div class="book-item">
                     <div class="flex flex-wrap items-center justify-between">
                         <div class="w-full flex-grow sm:w-auto">
-                            <a href="{{ route('books.show', $book) }}" class="book-title">Book Title</a>
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
                             <span class="book-author">by {{ $book->author }}</span>
                         </div>
                         <div>
