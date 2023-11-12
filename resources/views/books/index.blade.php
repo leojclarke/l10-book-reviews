ecd4a21fb7d8b61ee6a4fb2231059a9554364159
Leo Clarke <leojclarke@gmail.com>
2023-11-12 16:54:04 +0100

    Add filter presets for books list

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -8,10 +8,31 @@
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
         @csrf
         <input type="text" class="input h-10" name="title" placeholder="Search by title" value="{{ request('title') }}" />
-
+        <input type="hidden" name="filter" value="{{ request('filter') }}" />
         <button type="submit" class="btn">Search</button>
         <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular last month',
+                'popular_last_6months' => 'Popular last 6 months',
+                'highest_rated_last_month' => 'Highest rated last month',
+                'highest_rated_last_6months' => 'Highest rated last 6 months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+                class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+                {{ $label }}
+            </a>
+        @endforeach
+
+
+    </div>
 
     <ul>
         @forelse ($books as $book)
